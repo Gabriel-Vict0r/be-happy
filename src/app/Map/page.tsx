34b58e7bfc6d5c51e3648b6057006e815c28d398
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-12-14 21:50:47 -0300

    feat: add from a button for sigup a orphan

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -2,7 +2,9 @@ import MapComponent from "@/components/forMap/MapComponent";
 import SideMap from "@/components/forMap/SideMap";
 import dynamic from "next/dynamic";
 import React from "react";
-
+import Link from "next/link";
+import { IoIosAdd } from "react-icons/io";
+import { IoIosAddCircle } from "react-icons/io";
 const PageMap = () => {
   const MapWithNoSSR = dynamic(
     () => import("@/components/forMap/MapComponent"),
@@ -11,7 +13,7 @@ const PageMap = () => {
     }
   );
   return (
-    <main className="flex flex-row w-full h-full">
+    <main className="flex flex-row w-full h-full relative">
       <SideMap />
       <MapWithNoSSR />
     </main>
--- src/components/forMap/MapComponent.tsx
+++ src/components/forMap/MapComponent.tsx
@@ -5,11 +5,15 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import L from "leaflet";
+import Link from "next/link";
+import { IoIosAdd } from "react-icons/io";
+import { IoIosAddCircle } from "react-icons/io";
+
 const MapComponent = () => {
   const url = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=sk.eyJ1Ijoicm90Y2l2MDEwMTAxIiwiYSI6ImNscTV0MGNldzBrYjAybG14eXI5cnM5ZHYifQ.uh9eBIYmXQIuLjOtZqLpwA`;
 
   const markerIcon = L.icon({
-    iconUrl: "/icon-principal.svg",
+    iconUrl: "/Location.svg",
     iconSize: new L.Point(60, 65),
   });
   return (
@@ -18,26 +22,28 @@ const MapComponent = () => {
         center={[-12.13885, -44.988522]}
         zoom={17}
         scrollWheelZoom
-        style={{ height: "100%", width: "100%" }}
+        style={{ height: "100%", width: "100%", zIndex: 0 }}
       >
         <TileLayer url={url} />
         <Marker
           position={[-12.13885, -44.988522]}
           draggable={true}
           icon={markerIcon}
-          interactive={true}
+          interactive={false}
         >
           <Popup
             keepInView={true}
             position={[-12.13885, -44.988522]}
             className="text-5xl"
-          >
-            <div>
-              
-            </div>
-          </Popup>
+          ></Popup>
         </Marker>
       </MapContainer>
+      <Link
+        href="/"
+        className="absolute bottom-10 right-10 rounded-[20px] p-1 text-white text-5xl bg-blue z-10"
+      >
+        <IoIosAdd className="" />
+      </Link>
     </div>
   );
 };
