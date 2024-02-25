0038a3ff5079b71d94140c0433b10fd84f40d8bb
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2024-02-25 12:29:24 -0300

    feat: the controllers and middlewares was adjusted for receive a image array

--- backend/src/controllers/CreatePictureController.ts
+++ backend/src/controllers/CreatePictureController.ts
@@ -1,28 +1,36 @@
 import { Request, Response } from "express";
 import { CreatePictureService, IPicture } from "../services/CreatePictureService";
 import { UploadImageService } from "../services/UploadImageService";
+import { Picture } from "../entity/Picture";
 
 
-
+type TResult = Picture[]
 export class CreatePictureController {
     async handle(req: Request, res: Response) {
         //const objImage = req.body;
-        const { file } = req;
+        const { files } = req;
         const { id_orphanage } = req.body
-        const urlImage = `https://behappybucket.s3.amazonaws.com/${file.filename}`;
-        console.log(urlImage)
+        const lengthArr = files.length as number;
+        let resultFinal: TResult = [];
+        for (let index = 0; index < lengthArr; index++) {
+            let presentFile = files[index];
+            let urlImage = `https://behappybucket.s3.amazonaws.com/${presentFile.filename}`;
+            console.log(urlImage)
 
-        const service = new CreatePictureService();
-        const objPicture: IPicture = {
-            id_orphanage: id_orphanage,
-            url: urlImage
-        }
-        console.log(objPicture);
-        const result = await service.execute(objPicture);
+            const service = new CreatePictureService();
+            const objPicture: IPicture = {
+                id_orphanage: id_orphanage,
+                url: urlImage
+            }
+            console.log(objPicture);
+            const result = await service.execute(objPicture);
 
-        if (result instanceof Error) {
-            return res.status(400).send('erro na controller: ').json(result.message);
+            if (result instanceof Error) {
+                return res.status(400).send('erro na controller: ').json(result.message);
+            }
+            console.log('result', result)
+            resultFinal.push(result)
         }
-        return res.status(200).json(result);
+        return res.status(200).json(resultFinal.map((element) => element));
     }
 }
--- backend/src/middlewares/UploadImageMiddleware.ts
+++ backend/src/middlewares/UploadImageMiddleware.ts
@@ -1,13 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { UploadImageService } from "../services/UploadImageService";
-
+import { Express } from "express";
 
 
 export const uploadImageMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const { file } = req;
+    //const { file } = req;
+    const files = req.files;
+    //var number = 0;
+    const lengthArr = files.length as number;
     const uploadImageService = new UploadImageService();
     try {
-        await uploadImageService.execute(file);
+        for (let index = 0; index < lengthArr; index++) {
+            let file = files[index];
+            await uploadImageService.execute(file);
+        }
         return next();
     } catch (error) {
         return res.status(400).json({ message: error.message }).send('falha')
