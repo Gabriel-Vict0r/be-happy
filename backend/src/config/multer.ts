226af133fc800dcec5cca8b2b6e5e1aa94e7dd9e
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2024-04-07 13:46:23 -0300

    multer

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -2,11 +2,13 @@ import multer from "multer";
 import path from "path";
 import crypto from 'crypto'
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
-
+//console.log(tmpFolder)
 export default {
-    directory: tmpFolder,
+    directory: '/tmp/uploads',
     storage: multer.diskStorage({
-        destination: tmpFolder,
+        destination: (req, file, cb) => {
+            cb(null, '/tmp/uploads')
+        },
         filename(req, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;
