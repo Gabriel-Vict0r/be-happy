2156a2717c99d4839cce1d2e027a4ba64a47e165
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2024-05-04 20:44:52 -0300

    test

--- backend/src/services/GetPictureService.ts
+++ backend/src/services/GetPictureService.ts
@@ -1,9 +1,10 @@
-import { AppDataSource } from "../data-source";
+import { getDataSource } from "../data-source";
 import { Picture } from "../entity/Picture.entity";
 
 
 export class GetPictureService {
     async execute(id: string) {
+        const AppDataSource = await getDataSource();
         const repo = AppDataSource.getRepository(Picture);
 
         const pictures = await repo.find({
@@ -14,5 +15,6 @@ export class GetPictureService {
                 id_orphanage: id
             }
         })
+        return pictures;
     }
 }
