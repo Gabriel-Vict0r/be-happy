8ad6917907dfb0da5d8a5d97f4b2a1be3bb900ba
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2024-07-13 15:31:35 -0300

    merge

--- backend/src/services/UploadImageService.ts
+++
@@ -1,9 +0,0 @@
-import S3Storage from "../utils/S3Storage";
-
-export class UploadImageService {
-    async execute(file: Express.Multer.File) {
-        const s3Storage = new S3Storage();
-
-        await s3Storage.saveFile(file.filename);
-    }
-}
